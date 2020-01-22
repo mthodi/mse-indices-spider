50841ea1dc67f738eebaa114f5549a2e14becb98
Martin Thodi <martthodi238@gmail.com>
2020-01-23 00:25:09 +0200

    Cleaned config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 engine = create_engine("mysql+pymysql://{user}:{pw}@{url}/{db}".format(
-    user="martin",
-    pw="$coder238", 
-    url="mthodi.mysql.pythonanywhere-services.com",
-    db="mthodi$miyanda_db"))
+    user="",
+    pw="", 
+    url="",
+    db=""))
